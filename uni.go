53a6096e907d5dc6225c4fb97873e2f9b499c245
aiiisana <aisanaond@gmail.com>
2024-10-06 22:14:32 +0500

    commit

--- uni.go
+++ uni.go
@@ -1,38 +1,38 @@
-package main
+// package main
 
-// encapsulation
-type Person struct {
-	name    string
-	faculty string
-	age     int
-}
+// // encapsulation
+// type Person struct {
+// 	name    string
+// 	faculty string
+// 	age     int
+// }
 
-// constructor with setters
-func newPerson(name, faculty string, age int) {
-	return &Person{
-		name:    name,
-		faculty: faculty,
-		age:     age,
-	}
-}
+// // constructor with setters
+// func newPerson(name, faculty string, age int) {
+// 	return &Person{
+// 		name:    name,
+// 		faculty: faculty,
+// 		age:     age,
+// 	}
+// }
 
-// getters
-func (p *Person) GetName() string {
-	return p.name
-}
+// // getters
+// func (p *Person) GetName() string {
+// 	return p.name
+// }
 
-func (p *Person) GetFaculty() string {
-	return p.faculty
-}
+// func (p *Person) GetFaculty() string {
+// 	return p.faculty
+// }
 
-func (p *Person) GetAge() int {
-	return p.age
-}
+// func (p *Person) GetAge() int {
+// 	return p.age
+// }
 
-// inheritance
-type Student struct {
-	Person
-	year int
-}
+// // inheritance
+// type Student struct {
+// 	Person
+// 	year int
+// }
 
-func main() {}
+// func main() {}

a00198832049d1bb9623144981fe4b3d4f0e9fd0
aiiisana <123059393+aiiisana@users.noreply.github.com>
2024-10-20 18:04:14 +0500

    Delete midterm directory

--- midterm/backend/internal/models/user.go
+++
@@ -1,9 +0,0 @@
-package models
-
-type User struct {
-	ID       uint   `gorm:"primaryKey"`
-	Name     string `json:"name" gorm:"size:255"`
-	Email    string `json:"email" gorm:"unique"`
-	Password string `json:"password"`
-	Tasks    []Task `gorm:"foreignKey:UserID"`
-}
